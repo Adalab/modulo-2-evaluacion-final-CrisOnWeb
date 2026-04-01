e6065015b171cc0febc9ee7469ab2664717a1f8e
Cris <cristinaporteiro@gmail.com>
2026-04-01 18:49:56 +0200

    Created interaction of favorites:  add favorites and localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,24 @@
 'use strict';
+/* POSIBLES MEJORAS:
+  - Delegación de eventos de los li
+  - Refactorizar
+*/
+
 // QUERY SELECTORS
 const searchBtn = document.querySelector('.js_searchBtn');
 const searchInput = document.querySelector('.js_searchInput');
 const resultsUl = document.querySelector('.js_resultsUl');
+const favoritesUl = document.querySelector('.js_favoritesUl');
 
 //VARIABLES GLOBALES
 let searchedSeries = [];
-const favoriteSeries = [];
+let favoriteSeries = [];
 
 // FUNCIONES
 function getSeriesDataFromAPI(searchedItem) {
   fetch(`//api.tvmaze.com/search/shows?q=${searchedItem}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       searchedSeries = data.map((serie) => {
         return {
           id: serie.show.id,
@@ -24,7 +29,6 @@ function getSeriesDataFromAPI(searchedItem) {
             : 'https://placehold.co/210x295/e0fbfc/5f9ea0/?text=TV',
         };
       });
-      console.log(searchedSeries);
 
       renderAllSeries(searchedSeries);
     })
@@ -32,9 +36,21 @@ function getSeriesDataFromAPI(searchedItem) {
 }
 
 function renderOneSerie(dataSerie) {
+  // Busca en favoritos si existe la serie
+  const favsIndex = favoriteSeries.findIndex(
+    (serie) => serie.id === dataSerie.id,
+  );
+
+  // Creación de la clase para favoritos solo si se encuentra en favoritos
+  const favClass = favsIndex !== -1 ? 'results__item--favorite' : '';
+
   // Creación del <li>
   const liSerie = document.createElement('li');
-  liSerie.classList.add('results__item');
+  liSerie.classList.add('js_resultsLi', 'results__item');
+  // Añade la clase solo si favClass no es falsy (está vacío)
+  if (favClass) {
+    liSerie.classList.add(favClass);
+  }
   liSerie.dataset.id = `${dataSerie.id}`;
   resultsUl.appendChild(liSerie);
 
@@ -54,21 +70,124 @@ function renderOneSerie(dataSerie) {
 }
 
 function renderAllSeries(dataSeries) {
+  resultsUl.innerHTML = '';
+
   dataSeries.forEach((serie) => {
     renderOneSerie(serie);
+
+    const allResultsLi = document.querySelectorAll('.js_resultsLi');
+    allResultsLi.forEach((li) => {
+      li.addEventListener('click', handleClickLi);
+    });
   });
+}
+
+function renderOneFav(dataFav) {
+  // Creación de <li>
+  const liFav = document.createElement('li');
+  liFav.classList.add('favorites__item');
+  liFav.dataset.id = `${dataFav.id}`;
+  favoritesUl.appendChild(liFav);
+
+  // Creación de <img>
+  const imgFav = document.createElement('img');
+  imgFav.classList.add('favorites__img');
+  imgFav.src = `${dataFav.image}`;
+  imgFav.alt = `${dataFav.name}`;
+  liFav.appendChild(imgFav);
+
+  // Creación de <p>
+  const titleFav = document.createElement('p');
+  titleFav.classList.add('favorites__text');
+  const textTitle = document.createTextNode(`${dataFav.name}`);
+  titleFav.appendChild(textTitle);
+  liFav.appendChild(titleFav);
+
+  //Creación de <button>
+  const btnFav = document.createElement('button');
+  btnFav.classList.add('favorites__btn');
+  btnFav.innerHTML = `
+    <svg
+      class="icon-heart"
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      viewBox="0 -960 960 960"
+      width="24px"
+      fill="#e0fbfc">
+      <path
+        d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/>
+    </svg>
+
+    <svg
+      class="icon-broken"
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      viewBox="0 -960 960 960"
+      width="24px"
+      fill="#e0fbfc">
+      <path
+        d="M481-83Q347-218 267.5-301t-121-138q-41.5-55-54-94T80-620q0-92 64-156t156-64q45 0 87 16.5t75 47.5l-62 216h120l-34 335 114-375H480l71-212q25-14 52.5-21t56.5-7q92 0 156 64t64 156q0 48-13 88t-55 95.5q-42 55.5-121 138T481-83Zm-71-186 21-211H294l75-263q-16-8-33.5-12.5T300-760q-58 0-99 41t-41 99q0 31 11.5 62t40 70.5q28.5 39.5 77 92T410-269Zm188-48q111-113 156.5-180T800-620q0-58-41-99t-99-41q-11 0-22 1.5t-22 5.5l-24 73h116L598-317Zm110-363ZM294-480Z"/>
+    </svg> `;
+  liFav.appendChild(btnFav);
+}
+
+function renderAllFavourites(dataFavs) {
+  favoritesUl.innerHTML = '';
+
+  dataFavs.forEach((serie) => {
+    renderOneFav(serie);
+  });
+}
+
+// Trae los datos de localStorage a la página
+function getFromLocalStorage() {
+  const localStorageFavs = localStorage.getItem('favs');
+
+  if (localStorageFavs !== null) {
+    favoriteSeries = JSON.parse(localStorageFavs);
+  }
+}
+
+// Guarda los datos en el localStorage
+function setInLocalStorage() {
+  const stringifyFavSeries = JSON.stringify(favoriteSeries);
+  localStorage.setItem('favs', stringifyFavSeries);
 }
 
 //FUNCIONES DE EVENTOS
 function handleClickSearchBtn(event) {
   event.preventDefault();
   const searchedValue = searchInput.value.trim();
-  console.log(searchedValue);
 
   getSeriesDataFromAPI(searchedValue);
+}
+
+function handleClickLi(event) {
+  const clickedId = event.currentTarget.dataset.id;
+
+  const clickedSerie = searchedSeries.find((serie) => {
+    return serie.id === parseInt(clickedId);
+  });
+
+  if (clickedId !== undefined) {
+    const favsIndex = favoriteSeries.findIndex(
+      (serie) => serie.id === clickedSerie.id,
+    );
+
+    if (favsIndex === -1) {
+      favoriteSeries.push(clickedSerie);
+    }
+
+    setInLocalStorage();
+
+    renderAllFavourites(favoriteSeries);
+    renderAllSeries(searchedSeries);
+  }
 }
 
 //EVENTOS
 searchBtn.addEventListener('click', handleClickSearchBtn);
 
 // CÓDIGO DE INICIO
+getFromLocalStorage();
+renderAllFavourites(favoriteSeries);
